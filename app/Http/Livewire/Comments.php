28cecbb2a0aad117a4ea253b09a0721df0158246
danielconradie12 <118206323+danielconradie12@users.noreply.github.com>
2022-12-08 09:46:05 -0700

    Make comments and comment one component

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -13,6 +13,12 @@ class Comments extends Component
     public $form = [
         'body' => '',
     ];
+    public $showDiv = false;
+    public $form2 = [
+        'comment_id' => '',
+        'body' => '',
+    ];
+
     protected $listeners = ['$refresh'];
 
 
@@ -26,21 +32,75 @@ class Comments extends Component
             'body' => $this->form['body'],
             'status' => 'Active',
             'author_id' =>  Session::get('author_id'),
-        ]));
-
-         $response = (array) $response;
+        ]), true);
          array_push($this->comments, $response);
          session()->flash('message', 'Comment successfully added.');
+         $this->form['body'] = '';
+    }
 
-        // dd($this->comments);
-        //  COMMENTS is missing from response
+
+    public function addComment($comment_id)
+    {
+        $this->validate([
+            'form2.body' => ['required', 'string', 'max:255'],
+        ]);
+
+        $response = json_decode($this->injectApi()->post(getenv('API_SITE') . '/comments/type/comment/' . $comment_id, [
+            'body' => $this->form2['body'],
+            'status' => 'Active',
+            'author_id' =>  Session::get('author_id'),
+        ]));
+
+        $response = (array) $response;
+        array_push($this->comment, $response);
+        session()->flash('message', 'Comment successfully added.');
+        $this->emitUp('$refresh');
+    }
+
+
+    public function destroyComment($comment_id)
+    {
+        $response = $this->injectApi()->delete(getenv('API_SITE') . '/comments/' . $comment_id);
+        session()->flash('message', $response['message']);
+        return redirect()->to('post/' . $this->topic_id);
+    }
+
+
+
+    public function upvoteComment($comment_id)
+    {
+        $response = $this->injectApi()->post(getenv('API_SITE') . '/votes/up/comment/' . $comment_id, [
+            'author_id' =>  Session::get('author_id'),
+        ]);
+        if ($response->getStatusCode() <> 200) {
+            session()->flash('message', $response['message']);
+        }
+
+        if (isset($response['points'])) {
+            $this->score = $response['points'];
+        }
+    }
+
+
+    public function downvoteComment($comment_id)
+    {
+        $response = $this->injectApi()->post(getenv('API_SITE') . '/votes/down/comment/' . $comment_id, [
+            'author_id' =>  Session::get('author_id'),
+        ]);
+        if ($response->getStatusCode() <> 200) {
+            session()->flash('message', $response['message']);
+        }
+
+        if (isset($response['points'])) {
+            $this->score = $response['points'];
+        }
     }
 
 
     public function mount($topic_id)
     {
         $this->topic_id = $topic_id;
-        $this->comments = json_decode($this->injectApi()->get(getenv('API_SITE') . '/topics/' . $topic_id . '/comments'));
+        $this->comments =  json_decode($this->injectApi()->get(getenv('API_SITE') . '/topics/' . $topic_id . '/comments'), true);
     }
 
 
